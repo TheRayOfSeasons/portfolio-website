f0ea8a53597231985798b01f8690d9115b43c02b
Ray Lawrence Henri Sison <raylawrencehenrisison@gmail.com>
2024-02-03 23:24:37 +0800

    Modify: on load trigger

--- packages/frontend/src/js/main.ts
+++ packages/frontend/src/js/main.ts
@@ -3,15 +3,15 @@ import { BubbleScene } from './three-animations/scenes/bubble-scene';
 import { ThreeAnimations } from './three-animations/render-manager';
 import { ElementActivator, visibilityActivator } from './commons/element-activator';
 
-setTimeout(() => {
-  const curtains = document.querySelectorAll('.curtain');
-  for (const curtain of curtains) {
-    const activator = new ElementActivator(curtain);
-    activator.setActive(true);
-  }
-}, 500);
-// ThreeAnimations.onLoad(() => {
-// });
+ThreeAnimations.onLoad(() => {
+  setTimeout(() => {
+    const curtains = document.querySelectorAll('.curtain');
+    for (const curtain of curtains) {
+      const activator = new ElementActivator(curtain);
+      activator.setActive(true);
+    }
+  }, 500);
+});
 const animations = [
   async () => ThreeAnimations.init({
     name: 'hero',
